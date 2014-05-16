b8a5e4d744158773774c8a530e59035a996d4b19
Jordan Johnson <jordan.johnson.lz.108@gmail.com>
2014-05-16 17:10:56 -0500

    Updates

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -15,8 +15,13 @@ angular.module('starter.controllers', [])
 })
 
 
-.controller('PersonCtrl', function($scope, $stateParams) {
-	$scope.Person = [
-		{ title: 'Jordan', id: 1, cell: "2244060800"}
-	];
+.controller('PersonCtrl', function($scope) {
+  $scope.People = [
+    { title: 'Jordan', id: 1, cell: "224-406-0800" },
+    { title: 'Shelby', id: 2 },
+    { title: 'Anders', id: 3 },
+    { title: 'Kelsey', id: 4 },
+    { title: 'Steven', id: 5 },
+    { title: 'Amanda', id: 6 }
+  ];
 })
